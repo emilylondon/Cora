4d51f4daafb32ebd3ed0b5f3bab3fee81c4b5871
emilylondon <elondon@usc.edu>
2022-04-06 22:11:04 -0700

    image display?

--- hardware/main.py
+++ hardware/main.py
@@ -1,57 +1,56 @@
 import tkinter 
 import sys 
-from pynput import keyboard #sample inputs, will delete later
+from gpiozero import Button 
 from PIL import Image, ImageTk
 import time 
+
 
 feelArr = ['happy.png', 'sad.png', 'angry.png', 'excited.png']
 loc = 0
 
+#button declarations 
+leftButton = Button(20)
+rightButton = Button(16)
+selectButton = Button(21)
+
+#prolonged button press 
+
 def showPIL(imageName):
-    window = tkinter.Tk()
-
-    #set attributes 
-    window.attributes('-fullscreen', True)
-    window.title("Cora")
-
-    width = window.winfo_screenwidth()
-    height = window.winfo_screenheight()
     #display image
     display = ImageTk.PhotoImage(imageName)
     
     label1 = tkinter.Label(image=display)
     label1.image = display
 
-    label1.place(x=width/4,y=0)
-    window.mainloop()
-
-def on_press(key):
-    try: 
-        k = key.char
-    except: 
-        k = key.name
-    if k == 'd':
-        if loc!=3:
-            loc = loc + 1
-        else:
-            loc = 0 
-    if k == 'a':
-        if loc!=0:
-            loc = loc - 1
-        else: 
-            loc = 3
-    loc = 0
-    imageDisp = Image.open(feelArr[loc])
-    showPIL(imageDisp)
+    label1.place(x=0,y=0)
+  
 
 
 if __name__ == '__main__':
-    #setup the keyboard event listener
-    lis = keyboard.Listener(on_press=on_press)
-    lis.start() # start to listen on a separate thread
+    window = tkinter.Tk()
+    #set attributes 
+    #window.attributes('-fullscreen', True)
+    window.title("Cora")
+    height = window.winfo_screenheight()
 
     mainImage= Image.open("coraMain.png")
     showPIL(mainImage)
+    window.update()
 
-    while True:
-        time.sleep(1)
+    while True: 
+        if leftButton.is_pressed:
+            if (loc == 0):
+                loc=3
+            else: 
+                loc-=1
+            print("left!")
+        if selectButton.is_pressed: 
+            print("select!")
+            showPIL(feelArr[loc])
+        if rightButton.is_pressed:
+            if (loc == 3):
+                loc = 0
+            else:
+                loc+=1
+            print("right!")
+        window.update()
