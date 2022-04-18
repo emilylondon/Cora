3da76cd518b571121319727a5abb14e1785e563e
emilylondon <elondon@usc.edu>
2022-04-18 15:38:13 -0700

    Added LED code

--- hardware/gif_test.py
+++ hardware/gif_test.py
@@ -1,8 +1,11 @@
+from scipy.io.wavfile import read
+import numpy as np
 import tkinter as tk
 from PIL import Image, ImageTk
 from itertools import count, cycle
 import time 
 import queue 
+import pigpio
 from gpiozero import Button 
 import threading
 
@@ -18,15 +21,74 @@ imageName=imgPath+feelArr[0]
 soundName= soundPath + 'sleeping.mp3'
 loc = 0
 cnt =0
+flg = 0
 q = queue.Queue()
 s = queue.Queue()
+d = queue.Queue()
 
 #GPIO button stuff/hardware 
 leftButton = Button(16)
 rightButton = Button(21)
 selectButton = Button(20)
 
- 
+#GPIO LEDs 
+RED_PIN   = 16
+GREEN_PIN = 21
+BLUE_PIN  = 20
+
+#Initial color values 
+RED=255
+GREEN = 0
+BLUE = 0 
+
+#Audio processing info 
+samplerate=44100
+resolution=20
+spwin=samplerate/resolution
+
+#Initialize the pi for pigpio
+pi = pigpio.pi()
+
+#helper functions 
+def color_map(amp, color):
+    mapped = color*(amp/9000)
+    if mapped > 255:
+        mapped = 255
+    return mapped 
+
+def window_rms(a, window_size=2):
+    energy_list = []
+    for s in range(0, a.shape[0], window_size):
+        e = s + window_size
+        energy = np.sqrt(np.mean(a[s:e]**2))
+        energy_list.append(energy)
+    return energy_list
+def color_cycle():
+    global RED
+    global BLUE
+    global GREEN
+    global flg
+
+    while flg!=0:
+        for r in range(255):
+            RED = r
+            time.sleep(0.05)
+        for b in range(255, 0, -1):
+            BLUE = b
+            time.sleep(0.05)
+        for g in range(255):
+            GREEN = g
+            time.sleep(0.05)
+        for r in range(255, 0, -1):
+            RED = r 
+            time.sleep(0.05)
+        for b in range(255):
+            BLUE = b
+            time.sleep(0.05)
+        for g in range(255, 0, -1):
+            GREEN = g
+            time.sleep(0.05)
+
 class ImageLabel(tk.Label):
     """
     A Label that displays images, and plays them if they are gifs
@@ -35,6 +97,7 @@ class ImageLabel(tk.Label):
     def load(self, im):
         if isinstance(im, str):
             im = Image.open(im)
+            im = im.zoom(2, 2)    
         frames = []
  
         try:
@@ -71,8 +134,10 @@ def iterate_through(loc):
     loc=0
     cnt=0
     flag=0
+    global flg
     while True:
         if leftButton.is_pressed:
+            flg=0
             time.sleep(.005)
             if flag == 0: 
                 s.put(soundPath + feelArrAud[0])
@@ -85,6 +150,7 @@ def iterate_through(loc):
             while leftButton.is_pressed:
                 pass
         if rightButton.is_pressed:
+            flg=0
             time.sleep(.005)
             if flag == 0: 
                 s.put(soundPath + feelArrAud[0])
@@ -104,11 +170,14 @@ def iterate_through(loc):
                     loc=0
                     cnt=0
                     flag=0
+                    flg=0
                 cnt+=1 
                 time.sleep(0.02)       
             
             q.put(imgPath + actionArr[loc])
             s.put(soundPath + actionArrAud[loc ])
+            if loc==1:
+                flg=1
         time.sleep(0.02)
 
 def play_audio():
@@ -116,7 +185,29 @@ def play_audio():
         if (s.empty()!=True):
             audio = s.get()
             print(audio)
-    
+            a = read(audio)
+            r = np.array(a[1], dtype=float)
+            #2205 samples per window 
+            psong=window_rms(r, window_size=int(spwin))
+            d.put(psong)
+
+def play_lights():
+    while True:
+        if (d.empty()!=True):
+            psong=d.get()
+            time.sleep(1)
+            global RED
+            global BLUE 
+            global GREEN
+        
+            for t in range(len(psong)):
+                r = color_map(psong[t], RED)
+                g = color_map(psong[t], GREEN)
+                b = color_map(psong[t], BLUE)
+                pi.set_PWM_dutycycle(RED_PIN, r)
+                pi.set_PWM_dutycycle(GREEN_PIN, g)
+                pi.set_PWM_dutycycle(BLUE_PIN, b)
+                time.sleep(0.05)
 #demo :
 if __name__ == '__main__':
     root = tk.Tk()
@@ -124,6 +215,8 @@ if __name__ == '__main__':
     #root.attributes('-fullscreen', True)
     itTrd = threading.Thread(target=iterate_through, args=(loc,))
     audTrd = threading.Thread(target=play_audio)
+    cycTrd = threading.Thread(target= color_cycle)
+    lightTrd = threading.Thread(target= play_lights)
     lbl = ImageLabel(root)
     lbl.pack()
     lbl.load(imageName)
