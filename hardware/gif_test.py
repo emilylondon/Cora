625f36a6bf780c4edbc7c93e4d8758a262efbf6a
mcho9434 <mcho9434@usc.edu>
2022-04-20 21:24:43 -0700

    indexing bug

--- hardware/gif_test.py
+++ hardware/gif_test.py
@@ -1,12 +1,11 @@
 from scipy.io.wavfile import read
-import numpy as np
+
 import tkinter as tk
 from PIL import Image, ImageTk
 from itertools import count, cycle
 import time 
 import queue 
 import os 
-import pigpio
 from gpiozero import Button 
 import threading
 
@@ -23,9 +22,9 @@ soundName= soundPath + 'sleeping.mp3'
 loc = 0
 cnt =0
 flg = 0
-q = queue.Queue()
-s = queue.Queue()
-d = queue.Queue()
+q = queue.Queue() # iamge
+s = queue.Queue() # sound
+d = queue.Queue() # 
 
 #Initial screen width and height 
 w=0
@@ -36,39 +35,15 @@ leftButton = Button(16)
 rightButton = Button(21)
 selectButton = Button(20)
 
-#GPIO LEDs 
-RED_PIN   = 26
-GREEN_PIN = 19
-BLUE_PIN  = 13
-
-#Initial color values 
-RED=255
-GREEN = 0
-BLUE = 0 
 
 #loc color array 
-color=[[242,141,185], [255,255,0], [0,0,255], [255,0,0], [0,255,0]]
 
 #Audio processing info 
 samplerate=44100
 resolution=20
 spwin=samplerate/resolution
 
-
 #helper functions 
-def color_map(amp, color):
-    mapped = color*(amp/9000)
-    if mapped > 255:
-        mapped = 255
-    return mapped 
-
-def window_rms(a, window_size=2):
-    energy_list = []
-    for s in range(0, a.shape[0], window_size):
-        e = s + window_size
-        energy = np.sqrt(np.mean(a[s:e]**2))
-        energy_list.append(energy)
-    return energy_list
 
 def image_scale(im):
     imgWidth, imgHeight = im.size
@@ -77,32 +52,6 @@ def image_scale(im):
     imgHeight = int(imgHeight*ratio)
     im = im.resize((imgWidth,imgHeight))
     return im
-
-def color_cycle():
-    global RED
-    global BLUE
-    global GREEN
-    global flg
-
-    while flg!=0:
-        for r in range(255):
-            RED = r
-            time.sleep(0.05)
-        for b in range(255, 0, -1):
-            BLUE = b
-            time.sleep(0.05)
-        for g in range(255):
-            GREEN = g
-            time.sleep(0.05)
-        for r in range(255, 0, -1):
-            RED = r 
-            time.sleep(0.05)
-        for b in range(255):
-            BLUE = b
-            time.sleep(0.05)
-        for g in range(255, 0, -1):
-            GREEN = g
-            time.sleep(0.05)
 
 class ImageLabel(tk.Label):
     """
@@ -149,70 +98,62 @@ def iterate_through(loc):
     loc=0
     cnt=0
     flag=0
-    global flg
-    global RED
-    global GREEN
-    global BLUE
-    RED=color[loc][0]
-    GREEN=color[loc][1]
-    BLUE=color[loc][2]
-    print(RED)
-    print(BLUE)
-    print(GREEN)
-    pi.set_PWM_dutycycle(RED_PIN, RED)
-    pi.set_PWM_dutycycle(GREEN_PIN, GREEN)
-    pi.set_PWM_dutycycle(BLUE_PIN, BLUE)
+
     while True:
+
         if leftButton.is_pressed:
-            flg=0
+
             time.sleep(.005)
             if flag == 0: 
                 s.put(soundPath + feelArrAud[0])
-                flag = 1
-            if (loc==1):
-                loc=4
+                flag = 1 
+            if loc==0:
+                loc=1
+            elif (loc==1): 
+                loc=4 
             else:
-                loc-=1
+                loc-=1 
+
             q.put(imgPath + feelArr[loc])
             s.put(soundPath + feelArrAud[loc])
-            pi.set_PWM_dutycycle(RED_PIN, RED)
-            pi.set_PWM_dutycycle(GREEN_PIN, GREEN)
-            pi.set_PWM_dutycycle(BLUE_PIN, BLUE)
+
             while leftButton.is_pressed:
                 pass
         if rightButton.is_pressed:
-            flg=0
+
             time.sleep(.005)
             if flag == 0: 
                 s.put(soundPath + feelArrAud[0])
                 flag = 1
-            if (loc==4):
-                loc=2
+            if loc==0:
+                loc=1
+            elif (loc==4):
+                loc=1
             else:
                 loc+=1
+
             q.put(imgPath + feelArr[loc])
             s.put(soundPath + feelArrAud[loc])
-            pi.set_PWM_dutycycle(RED_PIN, RED)
-            pi.set_PWM_dutycycle(GREEN_PIN, GREEN)
-            pi.set_PWM_dutycycle(BLUE_PIN, BLUE)
+       
             while rightButton.is_pressed:
                 pass
         if selectButton.is_pressed:
             time.sleep(.005)
+            if loc==0:
+                loc=1
             while selectButton.is_pressed:
                 print(cnt)
                 if cnt==100:
                     loc=0
                     cnt=0
                     flag=0
-                    flg=0
+
                 cnt+=1 
                 time.sleep(0.02)       
             
             q.put(imgPath + actionArr[loc])
             s.put(soundPath + actionArrAud[loc])
-            if loc==1:
-                flg=1
+            
         time.sleep(0.02)
 
 def play_audio():
@@ -226,24 +167,6 @@ def play_audio():
             #2205 samples per window 
             psong=window_rms(r, window_size=int(spwin))
             d.put(psong)
-
-def play_lights():
-    while True:
-        if (d.empty()!=True):
-            psong=d.get()
-            time.sleep(1)
-            global RED
-            global BLUE 
-            global GREEN
-        
-            for t in range(len(psong)):
-                r = color_map(psong[t], RED)
-                g = color_map(psong[t], GREEN)
-                b = color_map(psong[t], BLUE)
-                pi.set_PWM_dutycycle(RED_PIN, r)
-                pi.set_PWM_dutycycle(GREEN_PIN, g)
-                pi.set_PWM_dutycycle(BLUE_PIN, b)
-                time.sleep(0.05)
 #demo :
 if __name__ == '__main__':
     #Initialize the pi for pigpio
