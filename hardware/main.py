38011fb034858433a805d6b366202dedf46e9c0a
emilylondon <elondon@usc.edu>
2022-04-23 12:30:37 -0700

    Cycle fix

--- hardware/main.py
+++ hardware/main.py
@@ -223,27 +223,27 @@ def play_lights():
             for r in range(255):
                 RED = r
                 pi.set_PWM_dutycycle(RED_PIN, RED)
-                time.sleep(0.05)
+                time.sleep(0.005)
             for b in range(255, 0, -1):
                 BLUE = b
                 pi.set_PWM_dutycycle(BLUE_PIN, BLUE)
-                time.sleep(0.05)
+                time.sleep(0.005)
             for g in range(255):
                 GREEN = g
                 pi.set_PWM_dutycycle(GREEN_PIN, GREEN)
-                time.sleep(0.05)
+                time.sleep(0.005)
             for r in range(255, 0, -1):
                 RED = r 
                 pi.set_PWM_dutycycle(RED_PIN, RED)
-                time.sleep(0.05)
+                time.sleep(0.005)
             for b in range(255):
                 BLUE = b
                 pi.set_PWM_dutycycle(BLUE_PIN, BLUE)
-                time.sleep(0.05)
+                time.sleep(0.005)
             for g in range(255, 0, -1):
                 GREEN = g
                 pi.set_PWM_dutycycle(GREEN_PIN, GREEN)
-                time.sleep(0.05)
+                time.sleep(0.005)
                 
         elif (d.empty()!=True):
             psong=d.get()
